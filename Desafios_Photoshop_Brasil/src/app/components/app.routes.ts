cc6fc843175c912ce2457b1a4196c0a0c8f3ba1c
LuizPauloJRM <luizpaulomedeiros77@hotmail.com>
2026-05-07 22:57:56 -0300

    feat: criando estrutura inicial login

--- Desafios_Photoshop_Brasil/src/app/components/app.routes.ts
+++ Desafios_Photoshop_Brasil/src/app/components/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
+import {LoginComponent} from "../pages/login/login.component";
 //Rotas da aplicação declaração das rotas
 export const routes: Routes = [
   {
     path:"",
     component:HomeComponent
-  }
+  },
+  {
+    path:"login",
+    component:LoginComponent
 
+  }
 ];
