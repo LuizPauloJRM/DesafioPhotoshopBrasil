72972965244b209fc62e7aa90f939210100e5075
LuizPauloJRM <luizpaulomedeiros77@hotmail.com>
2026-05-09 11:47:41 -0300

    Front end completo estrutura de Login

--- Desafios_Photoshop_Brasil/src/app/components/default-primary-input/default-primary-input.component.ts
+++ Desafios_Photoshop_Brasil/src/app/components/default-primary-input/default-primary-input.component.ts
@@ -1,19 +1,52 @@
-import { Component ,Input} from '@angular/core';
-import {ReactiveFormsModule} from "@angular/forms";
+import { Component, Input, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR, ReactiveFormsModule } from "@angular/forms";
 
-type InputTypes = "text" | "email"|"password"
-
+type InputTypes = "text" | "email" | "password";
 
 @Component({
   selector: 'app-default-primary-input',
   standalone: true,
   imports: [ReactiveFormsModule],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => DefaultPrimaryInputComponent),
+      multi: true
+    }
+  ],
   templateUrl: './default-primary-input.component.html',
   styleUrl: './default-primary-input.component.scss'
 })
-export class DefaultPrimaryInputComponent {
-  @Input() type: InputTypes= "text";
-  @Input() formName: string= "";
-  @Input() placeholder: string= "";
-  @Input() label: string= "";
+export class DefaultPrimaryInputComponent implements ControlValueAccessor {
+  @Input() type: InputTypes = "text";
+  @Input() placeholder: string = "";
+  @Input() label: string = "";
+  @Input() inputName: string = "";
+
+
+  value: string = '';
+  onChange: any = () => {};
+  onTouched: any = () => {};
+
+  // Método chamado quando o usuário digita no input (HTML)
+  onInput(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+    this.value = value;
+    this.onChange(value);
+  }
+
+  // --- Métodos da Interface ControlValueAccessor ---
+  writeValue(value: any): void {
+    this.value = value;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {}
 }
